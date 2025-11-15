db2b7548a7a317fdbf2f69d2187575c0e5e1317f
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-15 14:26:46 +0500

    feat: Define the class UtterneceHandler

---
+++ include/utterence-handler.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<vector>
+#include<string>
+
+class UtterenceHandler {
+private:
+    std::vector<std::string> _input;
+    std::vector<std::string> _response;
+public:
+    UtterenceHandler();
+    UtterenceHandler(const std::string &file_name);
+    bool storeUserInput(const std::string &file_name);
+    bool displayResponse(const std::string &input);
+};
