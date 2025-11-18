bbe85d0b1e49ca4a9fa50d70523b96efaa8bfe5e
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-18 20:34:39 +0500

    fix: Change some method declarations LoanSelection class

--- include/loan-selection.h
+++ include/loan-selection.h
@@ -10,7 +10,8 @@ public:
     LoanSelection();
     LoanSelection(const std::string &file_name);
     bool storeHomeLoans(const std::string &file_name);
-    bool displayHomeLoansForArea(std::vector<HomeLoan> home_loans, std::string area);
+    bool homeLoansInArea(const std::string &utterence);
+    bool selectHome();
     bool payPerMonth();
     bool monthlyInstallment();
 };
