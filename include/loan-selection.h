e6d271a21a2a365094c9c9011f39dc0111c6484b
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-19 21:59:28 +0500

    fix: Update the loan-selection class

--- include/loan-selection.h
+++ include/loan-selection.h
@@ -2,7 +2,6 @@
 #include<vector>
 #include<string>
 #include"home-loan.h"
-
 class LoanSelection {
 private:
     std::vector<HomeLoan> _home_loans;
@@ -10,8 +9,10 @@ public:
     LoanSelection();
     LoanSelection(const std::string &file_name);
     bool storeHomeLoans(const std::string &file_name);
-    bool homeLoansInArea(const std::string &utterence);
+    std::vector<HomeLoan> homeLoansInArea(const std::string &utterence);
     bool selectHome();
     bool payPerMonth();
     bool monthlyInstallment();
+    std::string calculateMonthlyPayment(const HomeLoan &loan) const;
+    void calculateInstallmentBreakdown(const HomeLoan &loan, std::vector<std::string> &installments, std::vector<std::string> &remaining_price, std::string &total_price, std::string &down_payment) const;
 };
