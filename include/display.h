e1672dc663075babb2fa8d32a78e495ad7692e3c
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-20 01:19:56 +0500

    Update the display.h

--- include/display.h
+++ include/display.h
@@ -2,6 +2,8 @@
 #include <vector>
 #include <string>
 #include "home-loan.h"
+#include "car-loan.h"
+#include "scooter-loan.h"
 
 class Display {
 public:
@@ -18,9 +20,12 @@ public:
     void displyLoanTypes(const std::string &utterence);
     void selectAreaDisplay(const std::string &utterence);
     void homeLoanDisplay(const std::vector<HomeLoan> &home_loan, int start = 0, int end = -1, const std::string &prompt = "");
+    void carLoanDisplay(const std::vector<CarLoan> &car_loans, int start = 0, int end = -1, const std::string &prompt = "");
+    void scooterLoanDisplay(const std::vector<ScooterLoan> &scooter_loans, int start = 0, int end = -1, const std::string &prompt = "");
     void selectHomeDisplay();
     void payPerMonthDisplay(const std::string &price);
     void isInstallmentPlanNeed(const std::string &prompt = "Do you want to view the installment breakdown? (yes/no)");
     void monthlyInstallmentDisplay(const std::string &total_price, const std::string &down_payment, const std::vector<std::string> &installments, const std::vector<std::string> &remaining_price);
+    void monthlyPlanDisplay(const std::string &total_price, const std::string &down_payment, const std::vector<std::string> &months, const std::vector<std::string> &installments, const std::vector<std::string> &remaining_price);
     void promptForInput(const std::string &text);
 };
