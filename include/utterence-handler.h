8a6876b73b4493fc1652b1470ebbf03d91195bbe
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-19 20:03:21 +0500

    change the utterence-handler.h

--- include/utterence-handler.h
+++ include/utterence-handler.h
@@ -3,13 +3,15 @@
 #include<string>
 class UtterenceHandler {
 private:
-    std::vector<std::string> _input;
-    std::vector<std::string> _response;
+std::vector<std::string> _input;
+std::vector<std::string> _response;
 public:
-    UtterenceHandler();
-    UtterenceHandler(const std::string &file_name);
-    std::string trim(const std::string &str);
-    std::string toLower(const std::string &str);
-    bool storeUserInputAndResponse(const std::string &file_name);
-    bool generateResponse(const std::string &input);
+UtterenceHandler();
+UtterenceHandler(const std::string &file_name);
+std::string trim(const std::string &str);
+std::string toLower(const std::string &str);
+bool storeUserInputAndResponse(const std::string &file_name);
+std::string generateResponse(const std::string &input);
+std::string getResponse(const std::string &key) const;
+std::string replacePlaceholder(const std::string &text, const std::string &placeholder, const std::string &value) const;
 };
