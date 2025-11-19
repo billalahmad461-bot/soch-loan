62af624c731b369f3e21614f57a6fcba08f0f16f
Bilal Ahmad <228117528+billalahmad461-bot@users.noreply.github.com>
2025-11-19 23:57:54 +0500

    add applcation-handler classs

---
+++ include/application-handler.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "application.h"
+#include <vector>
+#include <string>
+
+class ApplicationHandler {
+private:
+    std::vector<Application> applications;
+public:
+    const std::vector<Application>& getApplications() const { return applications; }
+    ApplicationHandler();
+    bool load(const std::string& file_name);
+    bool save(const std::string& file_name) const;
+    std::string generateNextId() const;
+    void add(const Application& app);
+    int countSubmitted(const std::string& cnic) const;
+    int countApproved(const std::string& cnic) const;
+    int countRejected(const std::string& cnic) const;
+    Application* getById(const std::string& id);
+    const Application* getById(const std::string& id) const;
+    void update(const Application& app);
+};
